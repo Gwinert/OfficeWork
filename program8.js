7f8b91b4874c8c16f4230b86e169a9365e601048
Gwinert <gwinert@gmail.com>
2017-02-09 12:52:37 -0700

    Update program8.js

--- program8.js
+++ program8.js
@@ -1,34 +1,10 @@
 var http = require('http');
 var stream = require('concat-stream');
-var fs = require('fs');
-var print = function (data) {
 
-    var result = data.toString();
-    console.log(result.length);
-    console.log(result);
-};
-
-var done = function (response) {
-
-    response.pipe(stream(print));
-    response.on('error', function (err) {
-        console.error(err);
-        process.exit(1);
-    });
-
-};
-/*http.get(process.argv[2], function (response) {
+http.get(process.argv[2], function (response) {
 	response.setEncoding('utf8');
-	function pipe(response, out) {
-		response.on("error", function(error) {
-		out.emit("error", error);
-		});
-		response.on("data", function(data) {
-		out.write(data);
-		});
-		response.on("end", function() {
-		out.emit("end");
-		});
-	};
-});
-*/
+    response.pipe(stream(function (data) { 
+		console.log(data.length)
+		console.log(data)
+    }));
+})
